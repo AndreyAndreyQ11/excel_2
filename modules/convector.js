e8fe45c29db82c353b13786575ca04bd277486c0
AndreyAndreyQ11 <aayy6491@gmail.com>
2023-07-02 19:42:25 +0300

    test3

--- modules/convector.js
+++ modules/convector.js
@@ -1,31 +1,47 @@
 //
+import unitOfMeasurement from "./unitOfMeasurement.js";
+
 export default function convector(cell, lastRow) {
   let cells = [];
 
   for (let i = 0; i <= lastRow; i++) {
+    let arrayQ1 = [];
+    const cellA1 = cell[String.fromCharCode(66) + i];
+
     //
-
-    const arreyQ1 = [];
-    for (let j = 0; j <= 3; j++) {
-      const cellAddress = String.fromCharCode(66 + j) + i;
-
-      let q1 = cell[cellAddress] ? cell[cellAddress].v : "";
-
-      arreyQ1.push(q1);
+    if (
+      cell[String.fromCharCode(66) + i] &&
+      cell[String.fromCharCode(66) + i].v !== "Найменування"
+    ) {
+      for (let j = 0; j <= 3; j++) {
+        const cellAddress = String.fromCharCode(66 + j) + i;
+        arrayQ1.push(cell[cellAddress] ? cell[cellAddress].v : "");
+      }
+    } else {
+      arrayQ1 = ["", "", "", ""];
     }
-
-    cells.push(arreyQ1);
+    cells.push(arrayQ1);
   }
-
-  console.log(cells);
   //
+
+  let cellsQ2 = cells.map((item) => {
+    if (item[1]) {
+      return unitOfMeasurement(item);
+    } else {
+      if (item[0]) {
+        return [`<strong>${item[0]}</strong>`, ...["", "", "", "", ""]];
+      } else {
+        return [...item, ...["", ""]];
+      }
+    }
+  });
+  //
+
   let text = "";
-  for (let i = 0; i < cells.length; i++) {
-    text += `<pre>${cells[i][0]}         ${cells[i][1]} ГРН/М.П.       ${cells[i][2]} М * ${cells[i][1]} = ${cells[i][3]}</pre>`;
+  for (let i = 0; i < cellsQ2.length; i++) {
+    text += `<pre>${cellsQ2[i][0]}         ${cellsQ2[i][1]} ${cellsQ2[i][4]}       ${cellsQ2[i][2]} ${cellsQ2[i][5]} ${cellsQ2[i][3]}</pre>`;
   }
 
-  // return `<pre>${B3}</br>${B4}                ${C4} ГРН/М.П.                     ${D4} М * ${C4} = ${E4}</pre></br>
-  // ${text}
-  // `;
+  //
   return `${text}`;
 }
--- script.js
+++ script.js
@@ -13,7 +13,6 @@ document
       let worksheet = XLSX.read(data, { type: "array" }).Sheets["По образцу"];
       var lastRow = XLSX.utils.decode_range(worksheet["!ref"]).e.r + 1;
 
-      console.log(lastRow);
       //
       document.getElementById("output").innerHTML = convector(
         worksheet,
@@ -23,3 +22,31 @@ document
 
     reader.readAsArrayBuffer(file);
   });
+//
+// let q1 = ["Арматурыа 10", 0, 0, 0];
+
+// let convectorrr = (item) => {
+//   const unitOfMeasurement = {
+//     Арматура: ["ГРН/М.П.", `М * ${item[1]} =`],
+//     Анкер: ["ГРН/ШТ", `ШТ * ${item[1]} =`],
+//     Бетон: ["ГРН/М2", `М2 * ${item[1]} =`],
+//   };
+
+//   function extractSubstring(str) {
+//     const spaceIndex = str.indexOf(" ");
+//     if (spaceIndex !== -1) {
+//       return str.substring(0, spaceIndex);
+//     } else {
+//       return str;
+//     }
+//   }
+//   //
+
+//   for (const key in unitOfMeasurement) {
+//     if (key.toLowerCase() == extractSubstring(item[0]).toLowerCase()) {
+//       return [...item, ...unitOfMeasurement[key]];
+//     }
+//   }
+//   return [...item, ...["\\\\\\", `\\\\\\ * ${item[1]} =`]];
+// };
+// console.log(convectorrr(q1));
